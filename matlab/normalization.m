a88e7d798973b67b5984f20a58d0bcd28ac7b919
kilarinikhil <nikhilkilari.m2@gmail.com>
2020-06-03 12:22:49 +0530

    trail

--- matlab/normalization.m
+++
@@ -1,6 +0,0 @@
-function kernelNormalized = normalization(kernel)
-
-kernelAvg = mean(mean(kernel));
-kernelNormalized = (kernel - kernelAvg) / (sum(sum(((kernel - kernelAvg) .^2))) ^ 0.5);
-
-end
--- py files/normalization.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def normalize(image):
-	mean = np.mean(image)
-	
-	meanSubtractedImage = image - mean
-	
-	return np.divide(meanSubtractedImage,np.power(np.sum(np.power(meanSubtractedImage,2)),0.5))
